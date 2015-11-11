c41b915db548e020717cfe13cd0c0dff12a4c470
SunNy <rudrozzal@gmail.com>
2015-11-12 02:01:05 +0600

    articles added

--- client/views/content/articleWindow/articles/articles.js
+++ client/views/content/articleWindow/articles/articles.js
@@ -1,11 +1,6 @@
 Template['articles'].helpers({
     'article' : function () {
-        return [
-            { 'text' : 'Uses trusted packages', 'title' : 'archive', 'path' : '#packages' },
-            { 'text' : 'Has a console tool', 'title' : 'terminal', 'path' : '#console-tool' },
-            { 'text' : 'Embraces HTML5', 'title' : 'html5', 'color' : 'hover-orange', 'path' : '#html5' },
-            { 'text' : 'Provides a structure', 'title' : 'folder', 'path' : '#structure' }
-        ];
+        return Articles.find({},{sort:{createdAt:-1}});
     },
 });
 
--- client/views/write_article.js
+++ client/views/write_article.js
@@ -3,9 +3,13 @@ Template.write_article.events({
         event.preventDefault();
         var title = event.target.title.value;
         var body = event.target.body.value;
-
+        var summary = event.target.summary.value;
+        var tags = event.target.tag.value.split(',');
+        console.log(tags);
         Articles.insert({
             title: title,
+            summary: summary,
+            tags:tags,
             body: body,
             createdAt: new Date(),
             author: Meteor.user()._id
