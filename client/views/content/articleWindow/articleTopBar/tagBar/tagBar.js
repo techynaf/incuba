da1b619857586427e775fdf9006190d22d59318f
Gulshan Jubaed Prince <Jubaed@Gulshans-MacBook-Air.local>
2015-11-12 03:29:48 +0600

    tags

--- client/views/content/articleWindow/articleTopBar/tagBar/tagBar.js
+++ client/views/content/articleWindow/articleTopBar/tagBar/tagBar.js
@@ -1,10 +1,19 @@
 Template['tagBar'].helpers({
     'tags' : function () {
-        var query = Articles.find();
-        console.log(query);
-        return query;
-    },
-});
+        var data = Articles.find({},{fields:{tags:1}}).fetch();
+        var mainArray = [];
+        for(key in data){
+            var array = data[key].tags;
+            for(key3 in array){
+                mainArray.push(array[key3]);
+            }
+        }
+
+        return _.uniq(mainArray);
+
+
+
+}});
 
 Template['tagBar'].events({
 });
