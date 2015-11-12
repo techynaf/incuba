f8d966b3bb11146e5bd965f8704e973a324e259c
Gulshan Jubaed Prince <Jubaed@Gulshans-MacBook-Air.local>
2015-11-12 12:41:54 +0600

    slug added

--- client/views/write_article.js
+++ client/views/write_article.js
@@ -12,9 +12,20 @@ Template.write_article.events({
             tags:tags,
             body: body,
             createdAt: new Date(),
+            slug: title.replace(/\s+/g, '-').toLowerCase() + '-' + randomString(10),
             author: Meteor.user()._id
         });
 
         console.log("article added");
     }
 });
+
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -12,15 +12,3 @@ Router.route('/profile', function () {
     this.render('profile');
     SEO.set({ title: 'Profile -' + Meteor.App.NAME });
 });
-//
-//Router.map(function(){
-//    this.route('profile', {
-//        path: '/profile',
-//        layoutTemplate: 'profileLayout'
-//    });
-    //
-    //this.route('write', {
-    //    path: '/write',
-    //    layoutTemplate: 'basicLayout'
-//    //});
-//});
