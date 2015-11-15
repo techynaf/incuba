4a3e0288a52201b45e7756149c603d412bb501f5
SunNy <rudrozzal@gmail.com>
2015-11-15 16:50:01 +0600

    search-source package added

--- client/views/content/articleWindow/articleTopBar/searchBar/searchBar.js
+++ client/views/content/articleWindow/articleTopBar/searchBar/searchBar.js
@@ -3,3 +3,11 @@ Template['searchBar'].helpers({
 
 Template['searchBar'].events({
 });
+
+var options = {
+    keepHistory: 1000 * 60 * 5,
+    localSearch: true
+};
+var fields = ['packageName', 'description'];
+
+PackageSearch = new SearchSource('packages', fields, options);
