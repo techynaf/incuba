83fbf2a73c459010f037062fb421ffc976608f8f
Sakib Hasan <ajobbalok22@gmail.com>
2015-11-21 22:08:01 +0600

    db seeder fixed prifle pic

--- server/startup/fixtures.js
+++ server/startup/fixtures.js
@@ -2,12 +2,12 @@ Meteor.startup(function(){
 
     if ( Meteor.users.find().count() === 0 ) {
         var users = [
-            {full_name: 'Admin', email: 'admin@incuba.com', password: 'password', roles: ['admin'] },
-            {full_name: 'Mentor', email: 'mentor@incuba.com', password: 'password', roles: ['mentor'] },
-            {full_name: 'Writer',email: 'writer@incuba.com', password: 'password', slug: 'writer-lsfb1398r', roles: ['writer'] },
-            {full_name: 'Sakib Hasan',email: 'ajobbalok22@gmail.com', password: '9134967', slug: 'sakib-hasan-lsfb1398r', roles: ['writer'] },
-            {full_name: 'Sunny',email: 'sunny@incuba.com', password: 'password', roles: ['writer'] },
-            {full_name: 'Prince',email: 'prince@incuba.com', password: 'password', roles: ['writer'] },
+            {full_name: 'Admin', email: 'admin@incuba.com', password: 'password', profilePicture: "avatar.gif",roles: ['admin'] },
+            {full_name: 'Mentor', email: 'mentor@incuba.com', password: 'password',profilePicture: "avatar.gif", roles: ['mentor'] },
+            {full_name: 'Writer',email: 'writer@incuba.com', password: 'password', profilePicture: "avatar.gif", slug: 'writer-lsfb1398r', roles: ['writer'] },
+            {full_name: 'Sakib Hasan',email: 'ajobbalok22@gmail.com', password: '9134967', profilePicture: "avatar.gif", slug: 'sakib-hasan-lsfb1398r', roles: ['writer'] },
+            {full_name: 'Sunny',email: 'sunny@incuba.com', password: 'password', profilePicture: "avatar.gif", roles: ['writer'] },
+            {full_name: 'Prince',email: 'prince@incuba.com', password: 'password', profilePicture: "avatar.gif", roles: ['writer'] },
             {full_name: 'User',email: 'user@incuba.com', password: 'password'}
         ];
 
@@ -15,7 +15,7 @@ Meteor.startup(function(){
             var userId = Accounts.createUser({
                 email: userData.email,
                 password: userData.password,
-                profile : {full_name: userData.full_name, slug: userData.slug}
+                profile : {full_name: userData.full_name, slug: userData.slug, profilePicture: userData.profilePicture}
             })
             Meteor.users.update({_id:userId}, {$set: {'emails.0.verified':true}});
             if(userData.roles){
