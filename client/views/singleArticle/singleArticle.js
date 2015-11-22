551dc915f23f82079c417f01e970736a721704fc
Sakib Hasan <ajobbalok22@gmail.com>
2015-11-22 22:56:57 +0600

    delete button added

--- client/views/singleArticle/singleArticle.js
+++ client/views/singleArticle/singleArticle.js
@@ -1,12 +1,44 @@
 Template['singleArticle'].helpers({
     'article' : function () {
         var articleSlug = slug;
-        console.log(articleSlug);
         var article =  Articles.find({slug: articleSlug}).fetch();
-        console.log(article);
         return article[0];
+    },
+    ownArticle: function(){
+        if(Accounts.user()) {
+            var articleSlug = slug;
+            var article = Articles.find({slug: articleSlug}).fetch();
+            if ( typeof article[0] === "undefined") {
+                console.log(typeof article[0]);
+            }else{
+                if(article[0].author == Accounts.user()._id){
+                    console.log("SET");
+                    Session.set("DeleteButtonShow", true);
+                }
+            }
+        }
+    },
+    checkOwner: function(){
+        if(Session.get("DeleteButtonShow")){
+            return true;
+        }else{
+            return false;
+        };
     }
 });
 
 Template['singleArticle'].events({
+    'click #deleteButton':function(){
+        console.log("delet clciked"+slug);
+        var articleSlug = slug;
+        var article =  Articles.find({slug: articleSlug}).fetch();
+        if ( typeof article[0] === "undefined") {
+
+        }else{
+            Articles.remove({"_id": article[0]._id});
+            Router.go('/');
+        }
+    }
 });
+
+Session.set("DeleteButtonShow", false);
